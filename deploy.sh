106cd1f0a73940bded81b8377858db07cf6ad627
Matt Coleman <matt3188@users.noreply.github.com>
2016-02-27 22:43:43 +0000

    Update deploy.sh
    Removed gulp build from deploy.sh script to save building twice before deploy

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@
 
 set -e # exit with nonzero exit code if anything fails
 
-# run our compile script, discussed above
-npm run-script build
-
 # go to the out directory and create a *new* Git repo
 cd dist
 git init
