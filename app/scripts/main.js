c87f49c298b377408af30d1cbfa81fb414230533
Matt Coleman <hello@matt-coleman.co.uk>
2016-03-07 16:05:14 +0000

    Added shadow to intro text and more text to intro

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,8 +15,7 @@
     },
     mainSections: {
       intro: {
-        class: 'intro',
-        hasShadow: false
+        class: 'intro'
       },
       cta: {
         class: 'cta-banner',
@@ -76,7 +75,7 @@
       twitter: 'https://twitter.com/Matt__Coleman',
       facebook: 'https://www.facebook.com/matt.coleman.562'
     },
-    intro: 'Short intro to introduce my self',
+    intro: 'My key skills include but are not limited too HTML, CSS3, Javascript/ jQuery, Wordpress development, Web Design, Cross browser compatibility and implementing JQuery Plugins. I hand code all my work using SublimeText and I adopt a DRY approach, implementing SMACSS along the way. I’m proficient with Adobe’s Creative suite including Photoshop and Illustrator and I love semantic and scalable code. I am a Front-end developer with a strong background in design. This has allowed me to bridge the gap between design and development and bring well rounded solutions to the user. I also have experience with JIRA and Agile methodology. I am passionate and creative with a keen eye for detail. Nothing makes me happier than a well thought out website paying particular attention to how a user can interact and navigate through a site. Summary of skills: HTML, CSS3/SASS, Javascript/ jQuery, Grunt/ Gulp, Git, Wordpress, Web Design.',
     skills: {
       'Wordpress': 80,
       'Javascript': 65,
