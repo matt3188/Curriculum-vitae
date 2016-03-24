53d53276e54ce9ddb0a2dca9639c94a4585eaeed
Matt Coleman <hello@matt-coleman.co.uk>
2016-03-24 17:29:16 +0000

    Added info object to info.js and removed from app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,9 +8,6 @@ var app = {
     }
   },
   mainSections: {
-    intro: {
-      class: 'intro'
-    },
     cta: {
       class: 'cta-banner',
       hasShadow: false
@@ -33,7 +30,6 @@ var app = {
   sections: {
     info: '.my-info',
     infoList: '.info-list',
-    intro: '.intro',
     cta: '.cta-banner',
     skills: '.professional-skills',
     skillsList: '.skill-list',
@@ -64,7 +60,6 @@ var app = {
     facebook: 'https://www.facebook.com/matt.coleman.562',
     downloadCV: ''
   },
-  intro: 'My key skills include but are not limited too HTML, CSS3, Javascript/ jQuery, Wordpress development, Web Design, Cross browser compatibility and implementing JQuery Plugins. I hand code all my work using SublimeText and I adopt a DRY approach, implementing SMACSS along the way. I’m proficient with Adobe’s Creative suite including Photoshop and Illustrator and I love semantic and scalable code. I am a Front-end developer with a strong background in design. This has allowed me to bridge the gap between design and development and bring well rounded solutions to the user. I also have experience with JIRA and Agile methodology. I am passionate and creative with a keen eye for detail. Nothing makes me happier than a well thought out website paying particular attention to how a user can interact and navigate through a site. Summary of skills: HTML, CSS3/SASS, Javascript/ jQuery, Grunt/ Gulp, Git, Wordpress, Web Design.',
   skills: {
     'Wordpress': 80,
     'Javascript': 65,
