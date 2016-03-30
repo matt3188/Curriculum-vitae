8c916bf4d2a1a4ed9dce12de65c105bdf3d81239
Matt Coleman <hello@matt-coleman.co.uk>
2016-03-30 11:02:09 +0100

    Set up experience object

--- app/scripts/experience.js
+++ app/scripts/experience.js
@@ -5,7 +5,18 @@
 (function() {
   'use strict';
 
-  var experience = {};
+  var experience = {
+    settings: {
+      sectionName: 'experience',
+      hasShadow: false,
+      class: 'experience',
+      listEl: 'ul',
+      listClass: 'horizontal-list experience-list clearfix'
+    },
+    selectors: {
+      jobListClass: 'experience-list'
+    }
+  };
 
   experience.init = function() {};
 
